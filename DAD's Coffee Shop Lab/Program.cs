650ad02af578356c2735bb662b60c4da9f01e0a5
kingdvd <davidvgoodwin@gmail.com>
2024-08-23 14:32:09 -0400

    Continue, Total/Tax
    Added  validator loops for quantity inputs, added loops to allow customer increase order, and created receipt method.

--- DAD's Coffee Shop Lab/Program.cs
+++ DAD's Coffee Shop Lab/Program.cs
@@ -3,6 +3,7 @@ using DAD_s_Coffee_Shop_Lab;
 using System;
 using System.ComponentModel;
 using System.Xml.Serialization;
+using StaticLecture;
 List<Cart> SCart = new List<Cart>();
 string filePath = "../../../coffee.txt";
 Console.WriteLine("Hello, World!");
@@ -31,50 +32,81 @@ if (!File.Exists(filePath))
     }
     tempCoffee.Close();
 }
-//Menu print
-Coffee.ListProducts();
-Console.WriteLine("Enter the Menu(number /Coffee Name)");
-string? selection = Console.ReadLine();
-int input;
-//input order
-if (int.TryParse(selection, out input))
+bool orderProgram = true;
+while (orderProgram)
 {
-    while (input > Coffee.products.Count || input < 1)
+    //Menu print
+    Coffee.ListProducts();
+    Console.WriteLine("Enter the Menu(number /Coffee Name)");
+    string? selection = Console.ReadLine();
+    int input;
+    //input order
+    if (int.TryParse(selection, out input))
     {
-        Console.WriteLine("Enter the valid input");
-        input = int.Parse(Console.ReadLine());
+        while (input > Coffee.products.Count || input < 1)
+        {
+            Console.WriteLine("Enter the valid input");
+            input = int.Parse(Console.ReadLine());
+        }
+        input--;
+        Console.WriteLine($"{Coffee.products[input]}");
+        Coffee selected = Coffee.products[input];
+        DisplayCart(selected);
     }
-    input--;
-    Console.WriteLine($"{Coffee.products[input]}");
-}
-    
-else if ((Coffee.products.Any(p => p.Name.Equals(selection, StringComparison.OrdinalIgnoreCase))))
-{
-    Coffee selected = Coffee.products.Find(p => p.Name.Equals(selection, StringComparison.OrdinalIgnoreCase));
-    Console.WriteLine($"{selected}");
-    DisplayCart(selected);
-    
-}
-else
-{
-    Console.WriteLine("InValid input ");
+
+    else if ((Coffee.products.Any(p => p.Name.Equals(selection, StringComparison.OrdinalIgnoreCase))))
+    {
+        Coffee selected = Coffee.products.Find(p => p.Name.Equals(selection, StringComparison.OrdinalIgnoreCase));
+        Console.WriteLine($"{selected}");
+        DisplayCart(selected);
+
+    }
+    else
+    {
+        Console.WriteLine("InValid input ");
+        continue;
+    }
+    orderProgram = Validator.GetContinue("Would you like to add another item to your order?", "yes", "no");
+    Console.Clear();
+    if (orderProgram == false)
+    {
+        Console.Clear();
+        GetTotal();
+        Console.WriteLine("Thank you! Come again!");
+    }
 }
 
-
-static void DisplayCart(Coffee order)
+static void GetTotal()
 {
-    Console.WriteLine("Enter the Quantity:");
-    int quantity = int.Parse(Console.ReadLine());
-    double total = order.Price * quantity;
-    Cart.orderList.Add(new Cart(quantity, order, total ));
+    double total = 0;
+    double tax = 0.06;
+    Console.WriteLine("DAD's Coffee Roasters Cafe");
     Console.WriteLine("================================");
     Console.WriteLine("Name".PadRight(16) + "Quantity\t" + "Subtotal");
-    foreach (Cart o in Cart.orderList)
+    foreach (Cart t in Cart.orderList)
     {
-        
-        Console.WriteLine($"{o.Product.Name.PadRight(16)} {o.Quantity}\t\t{o.Rate}");
-
-        
+        total += t.Rate;
+        Console.WriteLine($"{t.Product.Name.PadRight(16)}" + $"{t.Quantity}\t\t{t.Rate}");
     }
-
+    Console.WriteLine($"\nSubtotal: ${total}");
+    Console.WriteLine($"Sales Tax: {total * tax}");
+    Console.WriteLine($"\nTotal: {total + (total * tax)}");
 }
+
+    static void DisplayCart(Coffee order)
+    {
+        Console.WriteLine("Enter the Quantity:");
+        int quantity = Validator.GetPositiveInputInt();
+        double total = order.Price * quantity;
+        Cart.orderList.Add(new Cart(quantity, order, total));
+        Console.WriteLine("================================");
+        Console.WriteLine("Name".PadRight(16) + "Quantity\t" + "Subtotal");
+        foreach (Cart o in Cart.orderList)
+        {
+
+            Console.WriteLine($"{o.Product.Name.PadRight(16)} {o.Quantity}\t\t{o.Rate}");
+
+
+        }
+
+    }
